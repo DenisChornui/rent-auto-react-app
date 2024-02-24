45c9a6d2571ab27fdf5dff473bb3f2b8382ed863
DenisChornui <129185799+DenisChornui@users.noreply.github.com>
2024-02-24 13:09:16 +0100

    card details info

--- src/components/CarDetailsInfo/CarDetailsInfo.js
+++ src/components/CarDetailsInfo/CarDetailsInfo.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react';
 
-export const CarDetailsInfo = ({carAllInfo}) => {
-    
-    return (
-        <div>
-
-            <p>{carAllInfo.make}</p>
-            
-        </div>
-
-    )
-}
+export const CarDetailsInfo = ({ carAllInfo }) => {
+  return (
+    <div>
+      <img src={carAllInfo.img} alt={carAllInfo.make} />
+      <div>
+        <p>
+          {carAllInfo.make} <span>{carAllInfo.model}</span>, {carAllInfo.year}
+        </p>
+      </div>
+    </div>
+  );
+};
